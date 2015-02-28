3cb69d3142cef05f162b1dabae6dab7a00208cc4
rania sayed <raniasayed313@gmail.com>
2015-02-28 20:06:10 +0200

    trival algorithm not tested yet

---
+++ src/Banker.java
@@ -0,0 +1,87 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+
+public class Banker {
+
+	static int res=3; // res = resources , pr = processes
+	static int pr=5;
+	boolean flag;
+	int [] available= new int[res]; 
+	boolean [] finish= new boolean[pr];
+	static int [][] maximum = new int[pr][res];
+	static int [][] allocation = new int[pr][res];
+	static int [][] need = new int[pr][res]; 
+	
+	public void checkRequest(int request[],int i ,int j){
+		if(request[j]<=need[i][j] && request[j]<=available[j]){
+			available[j]-=request[j];
+			allocation[i][j]-=request[j];
+			need[i][j]-=request[j];
+		}
+	}
+	public boolean checksafety(){
+		for(int i=0;i<pr;i++){
+			for(int j=0;j<res;j++){
+			finish[i]=false;
+			if(need[i][j]<available[j]){
+				available[j]+=allocation[i][j];
+				finish[i]=true;
+			}
+			}
+			}
+			for(int i=0;i<pr;i++){
+				if(finish[i]){
+				flag=true;
+			}
+				else{
+					flag=false;
+					break;
+				}
+			}
+			if(flag){
+				return true;
+			}
+			return false;
+	}
+	
+	public static String read(FileInputStream in) throws IOException{
+		String s="",result="";
+		 try (BufferedReader reader = new BufferedReader(
+		            new InputStreamReader(in, Charset.defaultCharset()))) {
+		         
+			        int i=0;
+		            while ((s = reader.readLine()) != null) {
+		               result+=s;
+		               String parts[] = result.split(",");
+			            String[] nums =  result.split(",");
+			            for(int j=0;j<res;j++){
+			            	maximum[i][j]= Integer.parseInt(parts[j]);
+			            	}
+			            	result ="";
+			            	i++;
+			            }
+			     
+
+		            
+		            return (result);
+}
+}
+	//read Processes , Resources , Instances 
+	
+	
+	
+	/*for(int i=0;i<pr;i++)
+		finish[i] =false; //all array = false*/
+	public static void main(String[]args) throws IOException{
+		FileInputStream in =new FileInputStream("infile.txt");
+		read(in);
+		for(int i=0;i<pr;i++)
+			for(int j=0;j<res;j++)
+				System.out.println("max["+i+"]["+j+"]"+maximum[i][j]);
+}
+}
